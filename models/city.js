e09b6a78f22895ec88b61171e5ade93b71dd7194
Bryce Lively <bryce.lively@gmail.com>
2015-12-05 17:54:38 -0500

    working on ajax call for json to pop card divs

--- models/city.js
+++ models/city.js
@@ -2,6 +2,38 @@ require("../db/schema");
 var mongoose = require("mongoose");
 var CityModel = mongoose.model("City");
 
+function City(info) {
+    this.name = info.name;
+    this.gifUrl = info.gifUrl;
+    this.averageSalary = info.averageSalary;
+    this.numJobs = info.numJobs;
+    this.costOfLiving = info.costOfLiving;
+    this.livability = info.livability;
+}
+
+City.prototype = {
+    getWeather: function(){
+        var url = "http://api.wunderground.com/api/45d833eecf376b21/conditions/q/CA/San_Francisco.json";
+        $.getJSON(url).then(function(response){
+            console.log(response.current_observation.weather);
+            return response.current_observation.weather;
+        });
+    },
+    getIndeedApi: function() {
+        var url = "http://api.indeed.com/ads/apisearch?publisher=" + env.indeed + "&q=web+developer&l=" + encodeURI(this.name) + "&sort=&radius=25&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2";
+        $.getJSON(url, function(response) {
+            city.numJobs = response.totalresults;
+            return response.totalresults;
+        })
+    },
+    getGlassdoorApi: function(){
+
+    },
+    getLivability: function(){
+
+    }
+}
+
 module.exports = CityModel;
 
 //TODO replace with .info and move to public
