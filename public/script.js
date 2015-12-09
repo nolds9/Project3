7d425448fecaebcc5352abe872563d3e10d9b255
James Moon <mooniker@gmail.com>
2015-12-08 22:47:45 -0500

    Add comments

--- public/script.js
+++ public/script.js
@@ -1,9 +1,12 @@
-var cities; // What's wrong with storing city data here for global use?
+// What's wrong with storing city data here for global use?
+var cities;  // filled by ajax request in listCities()
 
+// Helper function to add nice-looking commas to long numbers
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
+// Converts "Cleveland index" to human-readable language
 function getCleveIndexString(index) {
   var indexRounded = Math.round(index * 100);
   if ( indexRounded > 95 && indexRounded < 105 ) {
@@ -15,8 +18,9 @@ function getCleveIndexString(index) {
   }
 }
 
-var clicks = 0;
+var clicks = 0; // counts the number of clicks on cities
 
+// helper function for ajax call to get weather JSON
 function updateWeather(cityName, $target) { // get custom JSON element for city
   $.getJSON('/weather/' + cityName.split(',')[0] + '/' + cityName.split(', ')[1], function(json) {
     var $weatherEl = $target.find('p.weather');
@@ -26,7 +30,8 @@ function updateWeather(cityName, $target) { // get custom JSON element for city
   });
 }
 
-function getCityData(cityName) {
+// helper function to get individual JSON data for given city string
+function getCityData(cityName) { // e.g. cityName == 'Washington, DC'
   for ( var i = 0; i < cities.length; i += 1 ) {
     if ( cities[i].name === cityName )
       return cities[i];
@@ -35,19 +40,18 @@ function getCityData(cityName) {
   return null;
 }
 
+// Helper function to make table with given dataset as key value pairs
 function getInfoTable(keyValuePairs) {
   var table = '<table class="info-table">';
-  // table += '<th>';
-  // for ( var i = 0; i < headingStringArray.length; i += 1 ) {
-  //   table += '<td>' + headingStringArray[i] + '</td>';
-  // }
   table += '</th>';
   for ( var j = 0; j < keyValuePairs.length; j += 1 ) {
-    table += '<tr><td>' + keyValuePairs[j][0] + '</td><td>' + keyValuePairs[j][1] + '</td></tr>';
+    table += '<tr><td>' + keyValuePairs[j][0] + '</td><td>' +
+    keyValuePairs[j][1] + '</td></tr>';
   }
   return table + '</table>';
 }
 
+// Puts city stats and picture of given city name in target contender space
 function setCityAsContender(cityName, $target) {
   var city = getCityData(cityName); // get the JSON for the city selected
   $target.empty();
@@ -65,6 +69,7 @@ function setCityAsContender(cityName, $target) {
   // other API updates go here
 }
 
+// Counts clicks, delegates rendering of contender cities to left or right
 function setContender(cityName) {
   // console.log(clicks);
   if ( clicks % 2 === 1 ) { // set contender on left
@@ -72,16 +77,17 @@ function setContender(cityName) {
     setCityAsContender(cityName, $('#contender-left'));
   } else { // set contender on right and compare...
     setCityAsContender(cityName, $('#contender-right'));
-
   }
 }
 
+// Populates cities on the bottom that can be selected with JSON as provided
 function listCities($target, citiesJson) {
-  if (citiesJson)
-    cities = citiesJson;
+  if (citiesJson) // If the data is provided, save it as global variable
+    cities = citiesJson; // for later use, maybe
 
-  $target.empty();
+  $target.empty(); // empty the target div before we (re-)popualte it
 
+  // Iterate through each city provided in the JSON
   for ( var i = 0; i < cities.length; i += 1 ) {
     // add h3 element for each city name to city section (target)
     $target.append('<h3>' + citiesJson[i].name + '</h3>');
@@ -99,12 +105,9 @@ function listCities($target, citiesJson) {
   }
 }
 
+$(document).ready( function() {
 
-
-
-
-$(document).ready( function() { // why does this all need to be in on ready?
-
+  // Seems that this jQuery selector must be set here
   var $citiesSection = $('#cities');
 
   $.getJSON('/cities.json', function(json) {
